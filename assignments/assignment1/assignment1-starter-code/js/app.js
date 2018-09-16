67818d30cc5f81384c9e2fac6a58cf49636ce12a
kunalpurimkbk <kunalpurimkbk@gmail.com>
2018-09-16 19:08:19 +0530

    Add code for Assignment 1

---
+++ assignments/assignment1/assignment1-starter-code/js/app.js
@@ -0,0 +1,53 @@
+(
+    function() {
+        angular.module("LunchCheck", [])
+
+                .controller("LunchCheckController", LunchCheckController);
+        LunchCheckController.$inject = ["$scope"];
+        function LunchCheckController($scope) {
+            $scope.listOfItems = "";
+            $scope.lunchStatus = "";
+            $scope.lunchStatusColor = "black";
+
+            $scope.onCheckButtonClicked = function() {
+                var numberOfItems = getNumberOfItems($scope.listOfItems);
+                var resultStr = "";
+                var resultColor = "";
+
+                if (numberOfItems == 0) {
+                    resultStr = "Please enter data first";
+                    resultColor = "red";
+                } else if (numberOfItems <= 3) {
+                    resultStr = "Enjoy!";
+                    resultColor = "green";
+                } else {
+                    resultStr = "Too much!";
+                    resultColor = "green";
+                }
+
+                $scope.lunchStatus = resultStr;
+                $scope.lunchStatusColor = resultColor;
+            }
+
+            var getNumberOfItems = function(str) {
+                if (str == '') {
+                    return 0;
+                }
+
+                var listOfItems = str.split(",");
+                var length = 0;
+
+                listOfItems.forEach(function(value) {
+                    var trimmedValue = value.trim();
+                    console.log("[" + trimmedValue + "]");
+                    if (trimmedValue != "") {
+                        length = length + 1;
+                    }
+                });
+                
+                console.log(length);
+                return length;
+            }
+        }
+    }
+)();
